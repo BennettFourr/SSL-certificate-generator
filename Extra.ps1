7ef2fdc145bae30a832797b2ddb78af817921b4b
Other_side <94060774+Bennett-Fourr@users.noreply.github.com>
2023-08-27 19:05:00 +0000

    Add files via upload

---
+++ Extra.ps1
@@ -0,0 +1,22 @@
+openssl genrsa -out cert-key.pem 4096
+
+$cn = Read-Host "Enter Common Name (CN) value"
+
+$command = "openssl req -new -sha256 -subj ""/CN=$cn"" -key cert-key.pem -out cert.csr"
+
+Invoke-Expression $command
+
+$dns = Read-Host "Enter DNS value"
+$ip = Read-Host "Enter IP value"
+
+$command = "echo ""subjectAltName=DNS:$dns,IP:$ip"" >> extfile.cnf"
+
+Invoke-Expression $command
+
+openssl x509 -req -sha256 -days 365 -in cert.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile extfile.cnf -CAcreateserial
+
+
+openssl pkcs12 -export -out certificate.pfx -inkey cert-key.pem -in cert.pem
+
+openssl x509 -outform der -in cert.pem -out cert.der
+
